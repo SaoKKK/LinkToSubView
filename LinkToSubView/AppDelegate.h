05e477bc767bc3ae1003fbb72fe1c2a2d9a7ab5f
Saori Kohno <saorikohno@yahoo.com>
2016-01-29 09:49:08 +0900

    Initial Commit

---
+++ LinkToSubView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LinkToSubView
+//
+//  Created by 河野 さおり on 2016/01/29.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
